ce835ac181cbf465fbd942ec10fed82f32ccc9fb
ricardiobraga <rcardiobraga@gmail.com>
2023-04-20 17:07:55 -0300

    criando entity Post con User aninhado

--- src/main/java/com/ricardo/workshopmongo/config/Instantiations.java
+++ src/main/java/com/ricardo/workshopmongo/config/Instantiations.java
@@ -1,30 +1,48 @@
 package com.ricardo.workshopmongo.config;
 
+import java.time.LocalDate;
+
+import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
+
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import com.ricardo.workshopmongo.domain.Post;
 import com.ricardo.workshopmongo.domain.User;
+import com.ricardo.workshopmongo.repository.PostRepository;
 import com.ricardo.workshopmongo.repository.UserRepository;
 
 @Configuration
 public class Instantiations implements CommandLineRunner {
 
+    DateTimeFormatter fmt =  DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
     @Autowired
     private UserRepository userRepository;
+
+    @Autowired
+    private PostRepository postRepository;
 
     @Override
     public void run(String... args) throws Exception {
 
         userRepository.deleteAll();
+        postRepository.deleteAll();
 
         User maria = new User(null, "Maria Brown", "maria@gmail.com");
         User alex = new User(null, "Alex Green", "alex@gmail.com");
         User bob = new User(null, "Bob Grey", "bob@gmail.com");
 
+        
+
+        Post post1 = new Post(null, LocalDate.parse("21/03/2018", fmt), "Partiu viagem", "Vou viajar, abraços!!!", maria );
+        Post post2 = new Post(null, LocalDate.parse("25/03/2018", fmt), "Bom dia!!", "Acordei Feliz hoje!", maria );
+
         userRepository.saveAll(Arrays.asList(maria, alex, bob));
+        postRepository.saveAll(Arrays.asList(post1, post2));
     }
     
 }
---
+++ src/main/java/com/ricardo/workshopmongo/domain/Post.java
@@ -0,0 +1,107 @@
+package com.ricardo.workshopmongo.domain;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document
+public class Post implements Serializable {
+
+    private static final long serialVersionUID  = 1L;
+
+    @Id
+    private String Id;
+    private LocalDate date;
+    private String title;
+    private String body;
+    private User author;
+
+    public Post  (){
+
+    }
+
+    public Post(String id, LocalDate date, String title, String body, User author) {
+        Id = id;
+        this.date = date;
+        this.title = title;
+        this.body = body;
+        this.author = author;
+        
+    }
+
+    public String getId() {
+        return Id;
+    }
+
+    public void setId(String id) {
+        Id = id;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    
+
+    public User getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(User author) {
+        this.author = author;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((Id == null) ? 0 : Id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Post other = (Post) obj;
+        if (Id == null) {
+            if (other.Id != null)
+                return false;
+        } else if (!Id.equals(other.Id))
+            return false;
+        return true;
+    }
+
+    
+
+    
+
+
+}
